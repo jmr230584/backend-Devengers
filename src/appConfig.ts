ef0079d15c00ced7a52647328b5dd5b2e9d0aad6
anaassis6 <analuassis6@gmail.com>
2025-04-23 16:51:43 -0300

    inicio da conexão com o banco de dados

---
+++ src/appConfig.ts
@@ -0,0 +1,13 @@
+const SERVER_ROUTES = {
+    LISTAR_CLIENTES: '/lista/cliente',
+
+    LISTAR_INGRESSO: '/lista/ingressos',
+
+    LISTAR_SESSAO: '/lista/sessao',
+
+    LISTAR_FILME: '/lista/filme',
+
+    LISTAR_SALA: '/lista/sala'
+}
+
+export {SERVER_ROUTES}
---
+++ src/routes.ts
@@ -0,0 +1,2 @@
+import { Express } from "express";
+import { SERVER_ROUTES } from "./appConfig";
